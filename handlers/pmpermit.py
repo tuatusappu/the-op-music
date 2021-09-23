5d0a25100e88f65ea9501d5689799eb135a9e1f2
ABHINETWORK1 <88935139+ABHINETWORK1@users.noreply.github.com>
2021-09-23 13:02:43 +0530

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -3,7 +3,7 @@ from pyrogram import filters
 from pyrogram.types import Chat, Message, User
 from config import BOT_USERNAME
 
-@USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
-async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"💕𝗛𝗲𝗿𝗲 𝗔𝘀𝘀𝗶𝘀𝘁𝗮𝗻𝗰𝗲 𝗢𝗳 𝐀𝐁𝐇𝐈\n✨𝗧𝗵𝗶𝘀 𝗕𝗼𝘁 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗱 𝗕𝘆 @BROKEN_KING_HERE\n🌟𝗗𝗼𝗻𝘁 𝗦𝗽𝗮𝗺 𝗛𝗲𝗿𝗲")
-  return                        
+
+
+
+
